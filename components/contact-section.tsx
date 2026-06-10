6d84985b2d289facfc127d730e3a1cf4b0755c9c
NhzIwd7Esmki7V8j <le-laboratoire1@proton.me>
2026-06-10 10:53:57 +0200

    Update contact-section.tsx

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -20,11 +20,6 @@ const admins = [
     handle: "@moderateur1labo",
     url: "https://t.me/moderateur1labo",
   },
-  {
-    name: "Le Labo",
-    handle: "@moderateur2_lelabo",
-    url: "https://t.me/moderateur2_lelabo",
-  },
 ]
 
 export function ContactSection() {
